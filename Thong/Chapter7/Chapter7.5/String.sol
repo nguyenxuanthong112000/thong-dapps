86c12b2ecdf2a8f5f8d60e79f49da71175ea7130
thong/dapps <81599903+nguyenxuanthong112000@users.noreply.github.com>
2021-03-30 08:23:37 +0700

    Add files via upload

---
+++ Thong/Chapter7/Chapter7.5/String.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.5.13;
+
+contract WorkingWithVariables{
+    uint256 public myUint;
+    
+    function setMyUint (uint _myUint) public {
+        myUint = _myUint;
+    }
+    
+    bool public myBool;
+    
+    function setMyBool (bool _myBool) public {
+        myBool = _myBool;
+    }
+    
+    uint8 public myunit8;
+    
+    function incrementUnit() public {
+        myunit8++;
+    }
+    
+    function decrementnit() public {
+        myunit8--;
+    }
+    
+    address public myAddress;
+    
+    function setAddress(address _myAddress) public {
+        myAddress = _myAddress;
+    }
+    
+    function setBalanceOfAddress() public view returns(uint){
+        return myAddress.balance;
+    }
+    
+    string public myString="Hello the real world!!!";
+    
+    function setMyString(string memory _myString) public{
+        myString = _myString;
+        
+    }
+}
